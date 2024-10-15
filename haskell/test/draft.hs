396883f52f2e1b7b37feab0c9aa731c05fc0d4d3
pedr0-laptop <pedrovbpereira20@gmail.com>
2024-10-14 21:13:22 -0300

    Played around with haskell durante a monitoria

---
+++ haskell/test/draft.hs
@@ -0,0 +1,25 @@
+
+module Operations where
+
+import Nat
+
+plus :: Nat -> Nat -> Nat
+n `plus` O = n
+n `plus` (S m) = S (n `plus` m)
+
+times :: Nat -> Nat -> Nat
+n `times` O = O
+n `times` (S m) = n `plus` (n `times` m)
+
+power :: Nat -> Nat -> Nat
+n `power` O = S O
+n `power` (S m) = n `times` (n `power` m)
+
+fact :: Nat -> Nat
+fact O = S O
+fact (S n) = S n `plus` fact n
+
+fib :: Nat -> Nat
+fib O = S O
+fib (S O) = S O
+fib (S (S n)) = fib n `plus` fib (S n)
