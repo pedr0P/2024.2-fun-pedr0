d550e6eef242df4ab5d11aec2442221715e51bd1
pedr0-laptop <pedrovbpereira20@gmail.com>
2024-10-21 14:45:37 -0300

    I don't remember what change this was, but I probably should commit it

--- haskell/test/Operations.hs
+++ haskell/test/Operations.hs
@@ -1,11 +1,19 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Operations where
 
 import Nat
+import Distribution.Compat.Prelude (Integral(div))
+
 
 plus :: Nat -> Nat -> Nat
 n `plus` O = n
 n `plus` (S m) = S (n `plus` m)
+
+-- sub :: Nat -> Nat -> Nat
+-- sub n O = n
+-- sub O m = m
+-- sub (S n) (S m) = sub n m
 
 times :: Nat -> Nat -> Nat
 n `times` O = O
@@ -24,12 +32,22 @@ fib O = S O
 fib (S O) = S O
 fib (S (S n)) = fib n `plus` fib (S n)
 
-sub :: Nat -> Nat -> Nat
-sub n O = n
-sub O m = m
-sub (S n) (S m) = sub n m
+-- diva :: Nat -> Nat -> Nat
+-- diva O n = O
+-- diva n m = S (diva (sub n m) m)
 
-diva :: Nat -> Nat -> Nat
-diva O n = O
-diva n m = S (diva (sub n m) m)
+min :: Nat -> Nat -> Nat
+min _ O = O
+min O _ = O
+min (S n) (S m) = S (min n m)
 
+max :: Nat -> Nat -> Nat
+max n O = n
+max O m = m
+max (S n) (S m) = S (max n m)
+
+-- div
+-- quot
+-- rem
+-- gcd
+-- lcm
